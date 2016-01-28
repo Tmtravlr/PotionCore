80712a946c97bad5d521df0015b8f4dc691a614d
Tmtravlr <tmtravlrsmail@gmail.com>
2016-01-28 14:57:32 -0700

    Cleaned up config loader
    -Made comments a bit cleaner with the defaults on a new line
    -Changed some ranges to make more sense with the potion they modify

--- src/main/java/com/tmtravlr/potioncore/ConfigLoader.java
+++ src/main/java/com/tmtravlr/potioncore/ConfigLoader.java
@@ -49,59 +49,59 @@ public class ConfigLoader {
 
 	/** Loads all the potion-specific config options. */
 	private static void loadPotionConfigs() {
-		PotionArchery.damageModifier = config.getFloat("Damage Modifier", PotionArchery.NAME, (float) PotionArchery.damageModifier, Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY,
-				"The projectile damage modifier applied by the " + PotionArchery.NAME + " potion per each amplifier level.");
+		PotionArchery.damageModifier = config.getFloat("Damage Modifier", PotionArchery.NAME, (float) PotionArchery.damageModifier, 0.0f, Float.POSITIVE_INFINITY,
+				"The projectile damage modifier applied by the " + PotionArchery.NAME + " potion per each amplifier level.\n");
 
 		PotionExplosion.explosionSize = config.getFloat("Explosion Size", PotionExplosion.NAME, PotionExplosion.explosionSize, 0.0f, Float.POSITIVE_INFINITY,
-				"The base explosion size for the " + PotionExplosion.NAME + " potion.");
+				"The base explosion size for the " + PotionExplosion.NAME + " potion.\n");
 
 		PotionExplosionSelf.explosionSize = config.getFloat("Explosion Size", PotionExplosionSelf.NAME, PotionExplosionSelf.explosionSize, 0.0f, Float.POSITIVE_INFINITY,
-				"The base explosion size for the " + PotionExplosionSelf.NAME + " potion.");
+				"The base explosion size for the " + PotionExplosionSelf.NAME + " potion.\n");
 
 		PotionFire.fireDuration = config.getInt("Fire Duration", PotionFire.NAME, PotionFire.fireDuration, 0, Integer.MAX_VALUE,
-				"The fire duration applied by the " + PotionFire.NAME + " potion for each amplifier level.");
+				"The fire duration applied by the " + PotionFire.NAME + " potion for each amplifier level.\n");
 
-		PotionKlutz.damageModifier = config.getFloat("Damage Modifier", PotionKlutz.NAME, (float) PotionKlutz.damageModifier, Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY,
-				"The projectile damage modifier applied by the " + PotionKlutz.NAME + " potion per each amplifier level.");
+		PotionKlutz.damageModifier = config.getFloat("Damage Modifier", PotionKlutz.NAME, (float) PotionKlutz.damageModifier, 0.0f, Float.POSITIVE_INFINITY,
+				"The projectile damage modifier applied by the " + PotionKlutz.NAME + " potion per each amplifier level.\n");
 
-		PotionLaunch.launchSpeed = config.getFloat("Launch Speed", PotionLaunch.NAME, (float) PotionLaunch.launchSpeed, Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY,
-				"The initial launch speed for the " + PotionLaunch.NAME + " potion per each amplifier level.");
+		PotionLaunch.launchSpeed = config.getFloat("Launch Speed", PotionLaunch.NAME, (float) PotionLaunch.launchSpeed, 0.0f, Float.POSITIVE_INFINITY,
+				"The initial launch speed for the " + PotionLaunch.NAME + " potion per each amplifier level.\n");
 
-		PotionLevitate.floatSpeed = config.getFloat("Float Speed", PotionLevitate.NAME, (float) PotionLevitate.floatSpeed, Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY,
-				"The float speed for the " + PotionLevitate.NAME + " potion per each amplifier level.");
+		PotionLevitate.floatSpeed = config.getFloat("Float Speed", PotionLevitate.NAME, (float) PotionLevitate.floatSpeed, 0.0f, Float.POSITIVE_INFINITY,
+				"The float speed for the " + PotionLevitate.NAME + " potion per each amplifier level.\n");
 
-		PotionRecoil.reflectDamage = config.getFloat("Damage Reflection", PotionRecoil.NAME, PotionRecoil.reflectDamage, Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY,
-				"The percent of damage reflected for the " + PotionRecoil.NAME + " potion per each amplifier level.");
+		PotionRecoil.reflectDamage = config.getFloat("Damage Reflection", PotionRecoil.NAME, PotionRecoil.reflectDamage, 0.0f, 0.9f,
+				"The percent of damage reflected for the " + PotionRecoil.NAME + " potion per each amplifier level.\n");
 
 		PotionRepair.repairTime = config.getInt("Repair Time", PotionRepair.NAME, PotionRepair.repairTime, 0, Integer.MAX_VALUE,
-				"The ticks between each repair from the " + PotionRepair.NAME + " potion.");
+				"The ticks between each repair from the " + PotionRepair.NAME + " potion.\n");
 
 		PotionRevival.reviveHealth = config.getFloat("Revive Health", PotionRevival.NAME, PotionRevival.reviveHealth, 0.0f, Float.POSITIVE_INFINITY,
-				"The amount of health recovered for each amplifier level when reviving from the " + PotionRevival.NAME + "\npotion per each amplifier level in half hearts.");
+				"The amount of health recovered for each amplifier level when reviving from the " + PotionRevival.NAME + "\npotion per each amplifier level in half hearts.\n");
 
 		PotionRust.damageTime = config.getInt("Damage Time", PotionRust.NAME, PotionRust.damageTime, 0, Integer.MAX_VALUE,
-				"The ticks between each durability reduction from the " + PotionRust.NAME + " potion.");
+				"The ticks between each durability reduction from the " + PotionRust.NAME + " potion.\n");
 
-		PotionSlowfall.maxSpeed = config.getFloat("Max Speed", PotionSlowfall.NAME, (float) PotionSlowfall.maxSpeed, Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY,
-				"The base maximum fall speed for the " + PotionSlowfall.NAME + " potion.");
+		PotionSlowfall.maxSpeed = config.getFloat("Max Fall Speed", PotionSlowfall.NAME, (float) PotionSlowfall.maxSpeed, 0.0f, Float.POSITIVE_INFINITY,
+				"The base maximum fall speed for the " + PotionSlowfall.NAME + " potion.\n");
 
 		PotionSpin.rotationSpeed = config.getFloat("Rotation Speed", PotionSpin.NAME, PotionSpin.rotationSpeed, 0.0f, Float.POSITIVE_INFINITY,
-				"The base maximum random rotation speed change for the " + PotionSpin.NAME + " potion.");
+				"The base maximum random rotation speed change for the " + PotionSpin.NAME + " potion.\n");
 
 		PotionStepup.increase = config.getFloat("Step Height Increase", PotionStepup.NAME, PotionStepup.increase, 0.0f, Float.POSITIVE_INFINITY,
-				"The step height increase per level of the " + PotionStepup.NAME + " potion.");
+				"The step height increase per level of the " + PotionStepup.NAME + " potion.\n");
 
 		PotionTeleport.teleportRange = config.getFloat("Teleport Radius", PotionTeleport.NAME, (float) PotionTeleport.teleportRange, 1.0f, Float.POSITIVE_INFINITY,
-				"The base maximum teleport radius for the " + PotionTeleport.NAME + " potion.");
+				"The base maximum teleport radius for the " + PotionTeleport.NAME + " potion.\n");
 
 		PotionTeleportSpawn.teleportDelay = config.getInt("Teleport Delay", PotionTeleportSpawn.NAME, PotionTeleportSpawn.teleportDelay, 0, Integer.MAX_VALUE,
-				"The number of ticks the player must stay still before teleporting with the " + PotionTeleportSpawn.NAME + "\npotion (with 20 ticks/second).");
+				"The number of ticks the player must stay still before teleporting with the " + PotionTeleportSpawn.NAME + "\npotion (with 20 ticks/second).\n");
 
 		PotionVulnerable.damageMultiplier = config.getFloat("Damage Multiplier", PotionVulnerable.NAME, (float) PotionVulnerable.damageMultiplier, 0.0f, Float.POSITIVE_INFINITY,
-				"The base damage multiplier for the " + PotionVulnerable.NAME + " potion.");
+				"The base damage multiplier for the " + PotionVulnerable.NAME + " potion.\n");
 
-		PotionWeight.speedReduction = config.getFloat("Speed Reduction", PotionWeight.NAME, PotionWeight.speedReduction, Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY,
-				"The base jump speed reduction for the " + PotionWeight.NAME + " potion. Normal jump speed is about 0.4 block/tick.");
+		PotionWeight.speedReduction = config.getFloat("Jump Height Reduction", PotionWeight.NAME, PotionWeight.speedReduction, 0.0f, Float.POSITIVE_INFINITY,
+				"The base jump speed reduction for the " + PotionWeight.NAME + " potion.\nNormal jump speed is about 0.4 block/tick.\n");
 	}
 	
 }
